7143137a7f68c912fea080bf4e5b2e108fa65ebc
Mang Yau <mangjieng@gmail.com>
2021-11-29 13:42:16 -0500

    add layers

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,36 +1,32 @@
 <template>
   <l-map style="height: 100%" :zoom="zoom" :center="center">
+    <l-control-layers ref="controls"></l-control-layers>
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
 
-    <l-marker
-      v-for="place in allPlaces"
-      :key="place.Name"
-      :lat-lng="[place.Latitudes, place.Longitudes]">
-        <l-popup>
-          <div v-html="generatePopupText(place)">
-          </div>
-        </l-popup>
-    </l-marker>
-
-    <l-marker
-      v-for="station in stations"
-      :key="station['Station Name']"
-      :lat-lng="[station.Latitudes, station.Longitudes]"
-      :icon="ttcIcon"
-      >
-
-        <l-popup>
-          <div>
-            <b>{{station['Station Name']}} Station\</b>
-          </div>
-        </l-popup>
-    </l-marker>
+    <l-layer-group 
+      v-for="category in categories"
+      :key="category.key"
+      ref="layers"
+      layerType="overlay"
+      :name="category.name"
+      :visible="true">
+        <l-marker
+          v-for="place in places[category.key]"
+          :key="place.Name"
+          :lat-lng="[place.Latitudes, place.Longitudes]"
+          :icon="getIcon(category)">
+            <l-popup>
+              <div v-html="generatePopupText(category, place)">
+              </div>
+            </l-popup>
+        </l-marker>
+    </l-layer-group>
   </l-map>
 </template>
 
 <script>
   import L from "leaflet";
-  import {LMap, LTileLayer, LMarker, LPopup,} from 'vue2-leaflet';
+  import {LMap, LTileLayer, LMarker, LPopup, LLayerGroup, LControlLayers} from 'vue2-leaflet';
 
   export default {
     name: 'Map',
@@ -40,6 +36,8 @@
       LTileLayer,
       LMarker,
       LPopup,
+      LLayerGroup,
+      LControlLayers,
       // LIcon,
     },
 
@@ -54,24 +52,70 @@
 
         ttcIcon: L.icon({
           iconUrl: require('/src/assets/ttc.svg'),
-          iconSize: [38, 95], // size of the icon
+          iconSize: [40, 20], // size of the icon
         }),
       }
     },
 
     methods: {
-      generatePopupText: function(data) {
+      generateTtcText: function(data) {
+        let result = `<b>TTC Station</b><br><b>Station Name:</b> ${data['Station Name']}`
+        return result
+      },
+      generatePopupText: function(category, data) {
         let result = '';
-        for (const [key, value] of Object.entries(data)) {
-          if (key !== 'Longitudes' || key !== 'Latitudes') {
-            result += `<b>${key}:</b> ${value}<br>`
-          }
+        if (category.key === 'ttc') {
+          result = this.generateTtcText(data);
+        } else {
+          result += `<b>${category.name}</b><br>`;
+          result += `<b>Name:</b> ${data.Name}<br>`;
+          result += `<b>Address:</b> ${data.Address}<br>`;
+          result += data['Opening hours'] ? `<b>Opening Hours:</b> ${data['Opening hours']}<br>` : '';
+          result += data['Phone Number'] ? `<b>Phone Number:</b> ${data['Phone Number']}<br>` : '<b>Phone Number:</b> Not available<br>';
+          result += data['Ethnicity'] ? `<b>Ethnicity:</b> ${data['Etnicity']}<br>` : '';
+          result += data['Region'] ? `<b>Region:</b> ${data['Region']}<br>` : '';
+          result += data['Closest TTC Stops'] ? `<b>Closest TTC Stops:</b> ${data['Closest TTC Stops']}<br>` : '';
+          result += data['Website'] ? `<b>Website:</b> <a href="https://${data['Website']}" target="_blank" rel="noopener noreferrer">${data['Website']}</a><br>` : '<b>Website:</b> Not available<br>';
+          result += data['Eligibility'] ? `<b>Eligibility:</b> ${data['Eligibility']}<br>` : '';
+          result += data['Community Initiatives'] ? `<b>Community Initiatives:</b> ${data['Community Initiatives']}<br>` : '';
         }
         return result;
+      },
+
+      getIcon: function(category) {
+        if (category.key === 'ttc') {
+          return this.ttcIcon;
+        } else {
+          return null;
+        }
       }
     },
 
     computed: {
+      categories: function() {
+        return [
+          {
+            name: 'Campus Eateries',
+            key: 'campus_eateries'
+          },
+          {
+            name: 'Food Pantry',
+            key: 'food_pantry',
+          },
+          {
+            name: 'Food Initiatives',
+            key: 'food_initiatives'
+          },
+          {
+            name: 'Grocery Stores',
+            key: 'grocery_stores'
+          },
+          {
+            name: 'TTC Stations',
+            key: 'ttc'
+          }
+        ]
+      },
       allPlaces: function() {
         return [
           ...this.places.campus_eateries,
